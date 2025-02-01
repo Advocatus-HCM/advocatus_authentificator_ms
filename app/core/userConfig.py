b9728c26b849350f12b8074bf570c563014e70fe
mbarrerag <mbarrerag@unal.edu.co>
2025-02-01 18:44:38 -0500

    Adding update feature by email

--- app/core/userConfig.py
+++ app/core/userConfig.py
@@ -1,24 +1,50 @@
 from fastapi import HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+from app.db.models import User
 
-async def update_user(user_data, user, db: AsyncSession):
-    try:
-   
-        if user_data.email:
-            user.email = user_data.email
-        if user_data.password:
-            user.password_hash = get_password_hash(user_data.password)
-        if user_data.role:
-            user.role = user_data.role
 
-        db.add(user)
-        await db.commit()
+async def update_user(user_data, db: AsyncSession):
+   try:
+       result = await db.execute(select(User).where(User.email == user_data.email))
+       user = result.scalars().first()
+       if not user:
+           raise HTTPException(
+               status_code=status.HTTP_404_NOT_FOUND,
+               detail="User not found"
+           )
+      
+       if user_data.password:
+           user.password_hash = get_password_hash(user_data.password)
+       if user_data.role:
+           user.role = user_data.role
 
-        return {"message": "User updated", "user": user}
-    
-    except Exception as e:
-        await db.rollback()
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail=f"An error occurred while updating the user: {str(e)}"
-        )
+
+       db.add(user)
+       await db.commit()
+
+
+       return {"message": "User updated", "user": user}
+  
+   except Exception as e:
+       await db.rollback()
+       raise HTTPException(
+           status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+           detail=f"An error occurred while updating the user: {str(e)}"
+       )
+
+
+async def delete_user(user, db: AsyncSession):
+   try:
+       user_to_delete = await db.get(user, user.id_user)
+       if user_to_delete:
+           await db.delete(user_to_delete)
+           await db.commit()
+           return True
+       return False
+   except Exception as e:
+       await db.rollback()
+       raise HTTPException(
+           status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+           detail=f"An error occurred while deleting the user: {str(e)}"
+       )
